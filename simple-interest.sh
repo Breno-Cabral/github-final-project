a84b00b35e10083af83ae6c7318f26f996faca3d
Breno-Cabral <70556920+Breno-Cabral@users.noreply.github.com>
2022-08-03 17:56:36 -0400

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,20 +1,20 @@
-#!/bin/bash \
+#!/bin/bash 
 # This script calculates simple interest given principal,
-# annual rate of interest and time period in years. \ \
+# annual rate of interest and time period in years. 
 
-# Do not use this in production. Sample purpose only. \ \
+# Do not use this in production. Sample purpose only. 
 
-# Author: Upkar Lidder (IBM) \
-# Additional Authors: \
-# Breno-Cabral (brenoscabral@gmail.com) \ \
+# Author: Upkar Lidder (IBM) 
+# Additional Authors: 
+# Breno-Cabral (brenoscabral@gmail.com) 
 
-# Input: \
-# p, principal amount \
-# t, time period in years \
-# r, annual rate of interest \ \
+# Input: 
+# p, principal amount 
+# t, time period in years 
+# r, annual rate of interest 
 
-# Output: \
-# simple interest = p\*t\*r \ \
+# Output: 
+# simple interest = p\*t\*r 
 
 echo "Enter the principal:"
 read p
